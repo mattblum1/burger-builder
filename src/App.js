8aa3e580637880f3c1754680ea86a5b226073a8e
Matt Blum <mblum@parts-unltd.com>
2020-09-29 16:20:41 -0500

    Finish routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Layout from './hoc/Layout/Layout';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
   render () {
@@ -11,7 +12,8 @@ class App extends Component {
       <div>
         <Layout>
           <Switch>
-            <Route path="/checkout" component={Checkout} />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
+import { Route } from 'react-router-dom';
 
 class Checkout extends Component {
     state = {
-        ingredients: {
-            salad: 1,
-            meat: 1,
-            cheese: 1,
-            bacon: 1
-        }
+        ingredients: null,
+        totalPrice: 0
     }
 
-    componentDidMount() {
+    componentWillMount() {
         const query = new URLSearchParams(this.props.location.search);
         const ingredients = {};
-        query.entries().map(([key, value]) => ingredients[key, value]);
-        // for (let param of query.entries()) {
-        //     // ['salad', '1']
-        //     ingredients[param[0]] = +param[1];
-        // }
-        this.setState({ingredients: ingredients});
+        let price = 0;
+        // query.entries().map(([key, value]) => ingredients[key, value]);
+        for (let param of query.entries()) {
+            // ['salad', '1']
+            if (param[0] === 'price') {
+                price += +param[1];
+            } else {
+                ingredients[param[0]] = +param[1];
+            }
+        }
+        console.warn('price', price);
+        this.setState({ ingredients, totalPrice: price });
     }
 
     checkoutCancelledHandler = () => {
@@ -34,10 +38,11 @@ class Checkout extends Component {
     render() {
         return (
             <div>
-                <CheckoutSummary 
+                <CheckoutSummary
                     ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler}/>
+                    checkoutContinued={this.checkoutContinuedHandler} />
+                <Route path={this.props.match.path + '/contact-data'} render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)} />
             </div>
         );
     }
